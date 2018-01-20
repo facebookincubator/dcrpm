4c38a818c2207177a36ceabbad8b5c48850ce095
Alvaro leiva <aleivag@gmail.com>
2018-01-19 22:04:34 -0800

    add __version__ file to dcrpm so it will be easy for setup.py and also add --version for `dcrpm --version`
    Testing:
    
    python3 setup.py dist -> produce a source dist
    python3 setup.py bdist_wheel -> produce a wheel
    
    then i installed the wheel (the thing that probbaly will be installed by most machines) and
    
    ```
    venv/bin/dcrpm --version
    dcrpm 0.0.2
    ```
    
    also verified that we can install a sdist...

---
+++ dcrpm/__version__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the GPLv2 license found in the LICENSE
+# file in the root directory of this source tree.
+#
+
+"""
+This is the version file, modify __version__ to update new versions,
+then you can import dcrpm.__version__ to get the current one.
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+
+__version__ = '0.0.2'
+
+sys.modules[__name__] = __version__

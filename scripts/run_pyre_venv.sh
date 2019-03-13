bb3875dccda12d3608d4de0ae375dbdd35e3ecd3
Davide Cavalca <dcavalca@fb.com>
2019-03-13 09:39:16 -0700

    Add missing copyright notice

--- scripts/run_pyre_venv.sh
+++ scripts/run_pyre_venv.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+#
+# Copyright (c) 2019-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the GPLv2 license found in the LICENSE
+# file in the root directory of this source tree.
 
 set -eu
 set -o pipefail

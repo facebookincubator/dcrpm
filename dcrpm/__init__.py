96032d5558eb3459ac5244e7f6e78e11e12ec0de
Lumir Balhar <lbalhar@redhat.com>
2022-06-16 11:37:37 +0200

    Do not modify sys.modules, move __version__ into __init__.py

--- dcrpm/__init__.py
+++ dcrpm/__init__.py
@@ -11,3 +11,5 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+
+__version__ = "0.6.3"  # type: str
